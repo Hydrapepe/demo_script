f2cbbf45420f5e49081278bb5f1185870c21e598
Skyrimus <Skyrimus@yandex.ru>
2021-06-03 12:36:28 +0300

    Update dc2_1.ps1

--- dc2_1.ps1
+++ dc2_1.ps1
@@ -63,7 +63,7 @@ New-GPO -Name "Firewall"
 Set-GPRegistryValue -Name "Firewall" -Key "HKLM\Software\Policies\Microsoft\WindowsFirewall" -ValueName PolicyVersion -Type DWord -Value 541
 Set-GPRegistryValue -Name "Firewall" -Key "HKLM\Software\Policies\Microsoft\WindowsFirewall\FirewallRules" -ValueName '{39201855-FA74-4AFC-8CBD-BF4C02E57738}' -Type String -Value 'v2.28|Action=Allow|Active=TRUE|Dir=In|Protocol=1|Name=test1in|' 
 Set-GPRegistryValue -Name "Firewall" -Key "HKLM\Software\Policies\Microsoft\WindowsFirewall\FirewallRules" -ValueName '{2BC88FE6-B8D9-4DB9-AB38-989CADA48E33}' -Type String -Value 'v2.28|Action=Allow|Active=TRUE|Dir=Out|Protocol=1|Name=testout|'
-New-GPLink -Name "Firewall" -Target "DC=kazan,DC=wsr" 
+New-GPLink -Name "Firewall" -Target "DC=spb,DC=wsr" 
 }
 if($Stage -eq 1) 
 {
