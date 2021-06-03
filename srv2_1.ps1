0e6fb098477865de7bfeee3a7175705d3d636e04
Hydrapepe <66299521+Hydrapepe@users.noreply.github.com>
2021-06-03 09:32:19 +0300

    remove trash

--- srv2_1.ps1
+++ srv2_1.ps1
@@ -11,23 +11,11 @@ Rename-Computer -NewName SRV2 -Force
 Restart-Computer -Force
 }
 function two {
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'C:\\srv2_1.ps1 -Stage 3'
-$trigger = New-ScheduledTaskTrigger -AtLogon
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "PEPETEST2"
 Unregister-ScheduledTask -TaskName "PEPETEST1" -Confirm:$false
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Install-WindowsFeature -Name NET-Framework-45-ASPNET
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementService
 Add-Computer -DomainName spb.wsr -Credential SPB\Administrator -restart -force
-}
-function three {
-Import-Module ServerManager
-Unregister-ScheduledTask -TaskName "PEPETEST2" -Confirm:$false
-Install-WindowsFeature DHCP -IncludeManagementTools
-Add-DHCPServerSecurityGroup -ComputerName $env:COMPUTERNAME
-Restart-Service dhcpserver
-Install-WindowsFeature -Name AD-Domain-Services
-Install-ADDSDomainController -Credential (Get-Credential) -DomainName "spb.wsr" -InstallDNS:$true -ReadOnlyReplica:$true -SiteName "Default-First-Site-Name" -Force:$true
 }
 if($Stage -eq 1) 
 {
@@ -37,8 +25,3 @@ if($Stage -eq 2)
 {
 two
 }
-if($Stage -eq 3) 
-{
-three
-}
-
