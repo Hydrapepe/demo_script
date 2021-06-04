28b5b280adde9f53ac038e0ce6ec1acb925f48f8
Hydrapepe <66299521+Hydrapepe@users.noreply.github.com>
2021-06-05 01:00:47 +0300

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,1 +1,5 @@
 cmd /c "echo rabotaet blyat"
+git clone https://github.com/Skyrimus/cdxb cb
+cd cb
+git clone https://github.com/Hydrapepe/demo_script
+cmd /c "cdbxpcmd --burn-data -folder:demo_script -iso:output.iso -format:iso -changefiledates"
