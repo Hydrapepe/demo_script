20b92696d2ab0e642358b9dee7349ac24fc4abb1
Skyrimus <Skyrimus@yandex.ru>
2021-06-07 20:23:30 +0300

    Fix formatting for hosts file

--- lin/LCLIA.sh
+++ lin/LCLIA.sh
@@ -2,14 +2,14 @@
 apt-cdrom add
 hostnamectl set-hostname L-CLI-A
 apt install -y tcpdump net-tools curl vim lynx isc-dhcp-common dnsutils nfs-common cifs-utils sshpass openssh-server bash-completion
-echo "172.16.20.10    l-srv   l-srv.skill39.wsr
-10.10.10.1      l-fw    l-fw.skill39.wsr
-172.16.50.2     l-rtr-a l-rtr-a.skill39.wsr
-172.16.55.2     l-rtr-b l-rtr-b.skill39.wsr
-172.16.200.61   l-cli-b l-cli-b.skill39.wsr
-20.20.20.5      out-cli out-cli.skill39.wsr
-20.20.20.100    r-fw    r-fw.skill39.wsr
-192.168.20.10   r-srv   r-srv.skill39.wsr
-192.168.10.2    r-rtr   r-rtr.skill39.wsr
-192.168.100.100 r-cli   r-cli.skill39.wsr
-10.10.10.10     isp" > /etc/hosts
+echo -e "\n172.16.20.10    l-srv   l-srv.skill39.wsr" >> /etc/hosts
+echo "10.10.10.1      l-fw    l-fw.skill39.wsr" >> /etc/hosts
+echo "172.16.50.2     l-rtr-a l-rtr-a.skill39.wsr" >> /etc/hosts
+echo "172.16.55.2     l-rtr-b l-rtr-b.skill39.wsr" >> /etc/hosts
+echo "172.16.200.61   l-cli-b l-cli-b.skill39.wsr" >> /etc/hosts
+echo "20.20.20.5      out-cli out-cli.skill39.wsr" >> /etc/hosts
+echo "20.20.20.100    r-fw    r-fw.skill39.wsr" >> /etc/hosts
+echo "192.168.20.10   r-srv   r-srv.skill39.wsr" >> /etc/hosts
+echo "192.168.10.2    r-rtr   r-rtr.skill39.wsr" >> /etc/hosts
+echo "192.168.100.100 r-cli   r-cli.skill39.wsr" >> /etc/hosts
+echo "10.10.10.10     isp" >> /etc/hosts
--- lin/RSRV.sh
+++ lin/RSRV.sh
@@ -16,16 +16,16 @@ mkdir /media/CentOS
 mount /dev/sr0 /media/cdrom
 mount /dev/sr1 /media/CentOS
 yum -y install tcpdump net-tools curl vim lynx dhclient bind-utils nfs-utils cifs-utils nano bash-completion mc
-echo "172.16.20.10    l-srv   l-srv.skill39.wsr
-10.10.10.1      l-fw    l-fw.skill39.wsr
-172.16.50.2     l-rtr-a l-rtr-a.skill39.wsr
-172.16.55.2     l-rtr-b l-rtr-b.skill39.wsr
-172.16.200.61   l-cli-b l-cli-b.skill39.wsr
-20.20.20.5      out-cli out-cli.skill39.wsr
-20.20.20.100    r-fw    r-fw.skill39.wsr
-192.168.20.10   r-srv   r-srv.skill39.wsr
-192.168.10.2    r-rtr   r-rtr.skill39.wsr
-192.168.100.100 r-cli   r-cli.skill39.wsr
-10.10.10.10     isp" > /etc/hosts
+echo -e "\n172.16.20.10    l-srv   l-srv.skill39.wsr" >> /etc/hosts
+echo "10.10.10.1      l-fw    l-fw.skill39.wsr" >> /etc/hosts
+echo "172.16.50.2     l-rtr-a l-rtr-a.skill39.wsr" >> /etc/hosts
+echo "172.16.55.2     l-rtr-b l-rtr-b.skill39.wsr" >> /etc/hosts
+echo "172.16.200.61   l-cli-b l-cli-b.skill39.wsr" >> /etc/hosts
+echo "20.20.20.5      out-cli out-cli.skill39.wsr" >> /etc/hosts
+echo "20.20.20.100    r-fw    r-fw.skill39.wsr" >> /etc/hosts
+echo "192.168.20.10   r-srv   r-srv.skill39.wsr" >> /etc/hosts
+echo "192.168.10.2    r-rtr   r-rtr.skill39.wsr" >> /etc/hosts
+echo "192.168.100.100 r-cli   r-cli.skill39.wsr" >> /etc/hosts
+echo "10.10.10.10     isp" >> /etc/hosts
 
 
