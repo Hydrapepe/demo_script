af3b44ccc0bb29d3872e197006237fbe89c63179
Skyrimus <Skyrimus@yandex.ru>
2021-06-08 11:03:48 +0300

    Update LRTRB.sh

--- lin/LRTRB.sh
+++ lin/LRTRB.sh
@@ -13,3 +13,5 @@ echo "192.168.20.10   r-srv   r-srv.skill39.wsr" >> /etc/hosts
 echo "192.168.10.2    r-rtr   r-rtr.skill39.wsr" >> /etc/hosts
 echo "192.168.100.100 r-cli   r-cli.skill39.wsr" >> /etc/hosts
 echo "10.10.10.10     isp" >> /etc/hosts
+sed '/hosts/d' -i /etc/nsswitch.conf
+echo -e 'hosts:\tdns files' >> /etc/nsswitch.conf
