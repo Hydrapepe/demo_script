aefd4d91db6344b452744064465723fbb0962f61
Skyrimus <Skyrimus@yandex.ru>
2021-06-08 14:27:48 +0300

    Update LRTRA.sh

--- lin/LRTRA.sh
+++ lin/LRTRA.sh
@@ -23,10 +23,14 @@ auto ens192
 iface ens192 inet static
 address 172.16.50.2
 gateway 172.16.50.1
+nameservers 172.16.20.10
+domain-search skills39.wsr
 netmask 255.255.255.252
 
 auto ens224
 iface ens224 inet static
 address 172.16.100.1
+nameservers 172.16.20.10
+domain-search skills39.wsr
 netmask 255.255.255.0" > /etc/network/interfaces
 systemctl disable --now apparmor
