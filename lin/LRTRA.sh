fbb016a55d521009f2816fd241f30f8ec2ed9d18
Skyrimus <Skyrimus@yandex.ru>
2021-06-09 17:36:44 +0300

    Update LRTRA.sh

--- lin/LRTRA.sh
+++ lin/LRTRA.sh
@@ -34,7 +34,6 @@ nameservers 172.16.20.10
 domain-search skills39.wsr
 netmask 255.255.255.0" > /etc/network/interfaces
 systemctl disable --now apparmor
-
 echo -e '
 #!/bin/bash
 ip link add dev lo1 type dummy
@@ -42,5 +41,32 @@ ip address add 1.1.1.1/32 dev lo1
 ip link set lo1 up' > /etc/loop.up
 chmod +x /etc/loop.up
 echo -e 'post-up /etc/loop.up' >> /etc/network/interfaces
+sed '/INTERFACESv4/d' -i /etc/default/isc-dhcp-server
+sed '/INTERFACESv6/d' -i /etc/default/isc-dhcp-server
+echo -e 'INTERFACESv4="ens192 ens224"' >> /etc/default/isc-dhcp-server
+sed '/authoritative/d' -i /etc/dhcp/dhcpd.conf
+echo -e 'authoritative;' >> /etc/dhcp/dhcpd.conf
+sed '/option domain-name/d' -i /etc/dhcp/dhcpd.conf
+echo -e 'option domain-name "skill39.wsr";' >> /etc/dhcp/dhcpd.conf
+sed '/option domain-name-servers/d' -i /etc/dhcp/dhcpd.conf
+echo -e 'option domain-name-servers "172.16.20.10";' >> /etc/dhcp/dhcpd.conf
+echo -e '
+subnet 172.16.50.0 netmask 255.255.255.252 {}
 
+subnet 172.16.100.0 netmask 255.255.255.0 {
+   range 172.16.100.65 172.16.100.75;
+   option routers 172.16.100.1;
+}
 
+subnet 172.16.200.0 netmask 255.255.255.0 {
+   range 172.16.200.65 172.16.200.75;
+   option routers 172.16.200.1;
+}
+
+host clib {
+  hardware ethernet 00:0c:29:a3:ad:bf;
+  fixed-address 172.16.200.61;
+}' >> /etc/dhcp/dhcpd.conf
+systemctl restart isc-dhcp-server
+systemctl enable isc-dhcp-server
+reboot
