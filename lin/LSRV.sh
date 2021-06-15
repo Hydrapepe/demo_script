f489bf4c5b7b1cd80a45620467f8c88c76c7bece
Skyrimus <Skyrimus@yandex.ru>
2021-06-15 03:13:20 +0300

    RSYNC блин блять

--- lin/LSRV.sh
+++ lin/LSRV.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-cdrom add
 hostnamectl set-hostname L-SRV
-apt install -y tcpdump net-tools curl vim lynx isc-dhcp-common dnsutils nfs-common cifs-utils sshpass openssh-server bash-completion bind9
+apt install -y tcpdump net-tools curl vim lynx isc-dhcp-common dnsutils nfs-common cifs-utils sshpass openssh-server bash-completion bind9 rsync
 echo -e "\n172.16.20.10    l-srv   l-srv.skill39.wsr" >> /etc/hosts
 echo "10.10.10.1      l-fw    l-fw.skill39.wsr" >> /etc/hosts
 echo "172.16.50.2     l-rtr-a l-rtr-a.skill39.wsr" >> /etc/hosts
@@ -98,3 +98,23 @@ if $hostname contains "L-FW" or $fromhost-ip contains "172.16.20.1" then {
 }
 ' > /etc/rsyslog.conf
 service rsyslog restart
+mkdir /opt/sync
+useradd mrsync
+echo mrsync:toor | chpasswd
+chown mrsync /opt/sync
+sed '/RSYNC_ENABLE/d' -i /etc/default/rsync
+echo -e 'RSYNC_ENABLE=TRUE' >> /etc/default/rsync
+echo -e '
+[data]
+\tuid=mrsync
+\tread only=false
+\tpath=/opt/sync
+\tauth users=sync
+\tsecrets file=/etc/rsyncd.secrets
+\thosts allow=L-CLI-A.skill39.wsr, L-CLI-B.skill39.wsr
+\thosts deny=*
+' > /etc/rsyncd.conf
+echo sync:parol666 > /etc/rsyncd.secrets
+chmod 400 /etc/rsyncd.secrets
+systemctl enable --now rsync
+systemctl restart rsync
