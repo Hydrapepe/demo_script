256ff32b0535fea38912823fe1c3ad7f3937fec7
te9zo <80409955+te9zo@users.noreply.github.com>
2021-06-17 13:20:20 +0300

    Update RFW.sh

--- lin/RFW.sh
+++ lin/RFW.sh
@@ -16,16 +16,16 @@ mkdir /media/CentOS
 mount /dev/sr0 /media/cdrom
 mount /dev/sr1 /media/CentOS
 yum -y install tcpdump net-tools curl vim lynx dhclient bind-utils nfs-utils cifs-utils nano bash-completion mc iptables iptables-services
-echo -e "\n172.16.20.10    l-srv   l-srv.itnsa39.wsr" >> /etc/hosts
+echo -e "\n172.16.20.10    l-srv   l-srv.demo2020.wsr" >> /etc/hosts
 echo "10.10.10.1      l-fw    l-fw.demo2020.wsr" >> /etc/hosts
-echo "172.16.50.2     l-rtr-a l-rtr-a.itnsa39.wsr" >> /etc/hosts
-echo "172.16.55.2     l-rtr-b l-rtr-b.itnsa39.wsr" >> /etc/hosts
-echo "172.16.200.61   l-cli-b l-cli-b.itnsa39.wsr" >> /etc/hosts
-echo "20.20.20.5      out-cli out-cli.itnsa39.wsr" >> /etc/hosts
-echo "20.20.20.100    r-fw    r-fw.itnsa39.wsr" >> /etc/hosts
-echo "192.168.20.10   r-srv   r-srv.itnsa39.wsr" >> /etc/hosts
-echo "192.168.10.2    r-rtr   r-rtr.itnsa39.wsr" >> /etc/hosts
-echo "192.168.100.100 r-cli   r-cli.itnsa39.wsr" >> /etc/hosts
+echo "172.16.50.2     l-rtr-a l-rtr-a.demo2020.wsr" >> /etc/hosts
+echo "172.16.55.2     l-rtr-b l-rtr-b.demo2020.wsr" >> /etc/hosts
+echo "172.16.200.61   l-cli-b l-cli-b.demo2020.wsr" >> /etc/hosts
+echo "20.20.20.5      out-cli out-cli.demo2020.wsr" >> /etc/hosts
+echo "20.20.20.100    r-fw    r-fw.demo2020.wsr" >> /etc/hosts
+echo "192.168.20.10   r-srv   r-srv.demo2020.wsr" >> /etc/hosts
+echo "192.168.10.2    r-rtr   r-rtr.demo2020.wsr" >> /etc/hosts
+echo "192.168.100.100 r-cli   r-cli.demo2020.wsr" >> /etc/hosts
 echo "10.10.10.10     isp" >> /etc/hosts
 sysctl -w net.ipv4.ip_forward=1 >> /etc/sysctl.conf
 sed '/PermitRootLogin/d' -i /etc/ssh/sshd_config
